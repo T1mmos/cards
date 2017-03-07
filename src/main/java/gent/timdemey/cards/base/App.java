4b9e346148bce833749c17c14aa7b087945b71cb
Timmos <tim.demey@gmail.com>
2017-03-07 12:04:20 +0100

    test

--- src/main/java/gent/timdemey/cards/base/App.java
+++ src/main/java/gent/timdemey/cards/base/App.java
@@ -14,7 +14,7 @@ public class App {
     public static void main(String[] args) {
         State state = SolitaireSetup.setup();
         Rules rules = new SolitaireRules();
-        
+         
         Command cmd = new TransferCommand(new TransferDef(new PileDef("local", SolitaireSorts.STOCK, 0), new PileDef("local", SolitaireSorts.TALON, 0), 3));
         System.out.println(cmd.isAllowed(state, rules));
     }
